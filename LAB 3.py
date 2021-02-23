e1568b08d8eb6b23fca8a06d443d85bcf2103858
Shanish <chaudharysanish54@gmail.com>
2021-02-23 09:38:28 +0545

    Initial commit

---
+++ LAB 3.py
@@ -0,0 +1,7 @@
+# write a program that takes numbers and prints their sum.every number is given on a seperate line
+num1=int(input('enter the first number'))
+num2=int(input('enter the second number'))
+num3=int(input('enter the third number'))
+
+sum=num1+num2+num3
+print('the sum of num sum is ',sum)
